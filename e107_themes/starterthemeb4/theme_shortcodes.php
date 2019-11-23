1703e99be04a790983aa6caa3251831b692519e2
Jimako <janamicek@gmail.com>
2019-11-23 13:47:28 +0100

    use core version

--- e107_themes/starterthemeb4/theme_shortcodes.php
+++ e107_themes/starterthemeb4/theme_shortcodes.php
@@ -15,10 +15,11 @@ class theme_shortcodes extends e_shortcode
 {
     var $override = true;
     var $file_extension = '.html';
-    var $sitetheme = 'bootstrap4';
-
-    private $pm_prefs = null;
-    private $pm = null;
+    var $sitetheme      = 'bootstrap4';
+    
+    private $pm_prefs       = null;
+    private $pm             = null;
+    private $userReg        = false;
     
     function __construct()
     {
@@ -27,13 +28,12 @@ class theme_shortcodes extends e_shortcode
             e107::includeLan(e_PLUGIN.'pm/languages/'.e_LANGUAGE.'.php');
             require_once(e_PLUGIN."pm/pm_func.php");
     
-            $this->pm = new pmbox_manager();
-    
+            $this->pm = new pmbox_manager();      
             $this->pm_prefs = $this->pm->prefs();	
         }
       
         $this->sitetheme = e107::getPref('sitetheme');
-  
+        $this->userReg  = defset('USER_REGISTRATION');
         if(e107::isInstalled('jmtheme')) 
         {
             $where = 'layout_theme = "'.$this->sitetheme.'" AND layout_mode = "'.THEME_LAYOUT.'" LIMIT 1 ';
@@ -48,7 +48,12 @@ class theme_shortcodes extends e_shortcode
         else $this->file_extension = ".php";           
     }
 
-	function sc_layout_header($parm)
+	/**
+	 * Special Header Shortcode for dynamic menuarea templates.
+	 * @shortcode {---HEADER---}
+	 * @return string
+	 */
+	function sc_header()
 	{    
 		$header = varset( $this->customlayout['layout_header'] , "header_default");	  
 		$headerpath = e_THEME. $this->sitetheme.'/headers/'.$header. $this->file_extension;
@@ -61,18 +66,24 @@ class theme_shortcodes extends e_shortcode
 		return $text;
     }		
   
-	function sc_layout_footer($parm)
+	/**
+	 * Special Footer Shortcode for dynamic menuarea templates.
+	 * @shortcode {---FOOTER---}
+	 * @return string
+	 */
+	function sc_footer()
 	{
 		$footer = varset( $this->customlayout['layout_footer'] , "footer_default");
 		$footerpath = e_THEME. $this->sitetheme.'/footers/'.$footer.$this->file_extension;
-
 		if(file_exists($footerpath)) {
 		    $text = file_get_contents($footerpath); 
-            $text = e107::getParser()->parseTemplate($text);      
+             if(USER_AREA AND !$_GET['configure']) { 
+						   $text = e107::getParser()->parseTemplate($text); 
+					   }     
 		} 
         else $text = '';
 		return $text;
-    }	    
+   }	    
 }
 
 
