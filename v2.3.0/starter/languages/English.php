169898a0a685cf0b6221f4dd6ea57da1900e681e
Jimako <janamicek@gmail.com>
2020-07-02 15:44:52 +0200

    langs from FR

---
+++ v2.3.0/starter/languages/English.php
@@ -0,0 +1,39 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - 
+|
+
++----------------------------------------------------------------------------+
+*/
+
+define("LAN_THEME_REL_01", "You Might Also Like Those");
+define("LAN_THEME_NAV_01", "MENU");
+ 
+
+
+/* examples */
+define("LAN_FL_THEME_00", "Name");
+define("LAN_FL_THEME_01", "Please enter your name.");
+define("LAN_FL_THEME_02", "Email Address"); 
+define("LAN_FL_THEME_03", "Please enter your email address.");
+define("LAN_FL_THEME_04", "Phone Number");
+define("LAN_FL_THEME_05", "Please enter your phone number.");
+define("LAN_FL_THEME_06", "Message");
+define("LAN_FL_THEME_07", "Please enter a message.");
+define("LAN_FL_THEME_08", "Phone Number");
+define("LAN_FL_THEME_09", "Please enter your phone number.");
+define("LAN_FL_THEME_10", "Menu");
+
+
+
+define("LAN_FL_THEME_10B", "Around the Web");
+define("LAN_FL_THEME_11", "Contact Info");
+define("LAN_FL_THEME_12", "Close Window");
+define("LAN_FL_THEME_13", "Download Theme");
+define("LAN_FL_THEME_14", "Send");
+define("LAN_FL_THEME_15", "Posted on");
+define("LAN_FL_THEME_16", "by");
+define("LAN_FL_THEME_16", "Leave a Comment:");
+
+ 
---
+++ v2.3.0/starter/languages/English_admin.php
@@ -0,0 +1,51 @@
+<?php
+ /*
+* e107 website system
+*
+* Copyright (C) 2008-2013 e107 Inc (e107.org)
+* Released under the terms and conditions of the
+* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+*
+* e107 Freelancer Theme.
+*
+#######################################
+#     e107 website system theme       #
+#     by Jimako                    	  #
+#     https://www.e107sk.com          #
+#######################################
+*/
+ 
+/*  universal theme LANs don't remove them please */
+define("LAN_ADMIN_THEMEPREF_01", "Inline CSS <br> between head tags");
+define("LAN_ADMIN_THEMEPREF_02", "");     
+//define("LAN_ADMIN_THEMEPREF_03", "Iframe code for google maps on Contact Page");
+define("LAN_ADMIN_THEMEPREF_04", "Display standard menu with card look");
+define("LAN_ADMIN_THEMEPREF_05", "Header image"); 
+
+
+define("LAN_JM_ADMIN_HELP_01", "Help & Support");
+define("LAN_JM_ADMIN_HELP_02", "The support forum for this item is available on:  ");
+define("LAN_JM_ADMIN_HELP_03", "Support Forum");
+define("LAN_JM_ADMIN_HELP_04", "There is documentation too. Something is missing or not clear? Create a question on the forum, we will add the answer asap.:   ");
+define("LAN_JM_ADMIN_HELP_05", "Documentation");
+define("LAN_JM_ADMIN_HELP_06", "Live preview:   ");
+define("LAN_JM_ADMIN_HELP_07", "Demo");
+define("LAN_JM_ADMIN_HELP_08", "Github repository:  ");
+define("LAN_JM_ADMIN_HELP_09", "Github");
+define("LAN_JM_ADMIN_HELP_10", "Download. There can be a newer version available than on e107.org   ");
+define("LAN_JM_ADMIN_HELP_11", "Download");
+
+define("LAN_JM_THEME_OPTIONS", "Theme Options");
+
+define("LAN_JM_THEMEOPTIONS_LAN_01", "Theme Settings");                   
+define("LAN_JM_THEMEOPTIONS_LAN_02", "Custom CSS");      
+define("LAN_JM_THEMEOPTIONS_LAN_03", "CSS Editor");      
+define("LAN_JM_THEMEOPTIONS_LAN_04", '<p>Please add all your custom CSS here for simplier testing css changes. If you modify theme css directly, your changes will be lost in an upgrade.</p><p>Your custom CSS will be loaded after the theme\'s stylesheets, which means that your rules will take precedence.</p><p>Just add your CSS here for what you want to change, you don\'t need to copy all the theme\'s style.css content.</p>
+<p>Be aware that you can add file with name custom_style.css and put your css changes there. This file is loaded as last file from theme files and it is not part of upgrade pack, so it will not be overriden with update.');
+
+define("LAN_JM_THEMEOPTIONS_LAN_05", "Typography");
+define("LAN_JM_THEMEOPTIONS_LAN_06", " This is how your site will show the <strong>text by default</strong>, while you can change the typography settings for most elements separately. Note the font size will affect all elements in \"rem\" units, that is, almost all areas of your site.");
+define("LAN_JM_THEMEOPTIONS_LAN_07", "Font Size");
+    
+    
+	
